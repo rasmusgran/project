ca926eff87488f34f7bdc9153eb260f5680b654a
rasmusgran <83310031+rasmusgran@users.noreply.github.com>
2021-04-30 23:40:58 +0200

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
-plt.rcParams["font.family"] = 'Arial'
+plt.rcParams["font.family"] = 'Sans'
 
+plt.grid(color='#2A3459',linewidth=0.1)  # bluish dark grey, but slightly lighter than background
 
 def read_col(fname, col, convert=int, sep=None):
     with open(fname) as fobj:
@@ -15,12 +16,13 @@ y2 = read_col("Data/1000/Osorterad_Mergesort.txt", col=1)
 x3 = read_col("Data/1000/Osorterad_Timsort.txt", col=0)
 y3 = read_col("Data/1000/Osorterad_Timsort.txt", col=1)
 
-plt.plot(x1, y1, label = "Quicksort")
-plt.plot(x2, y2, label = "Mergesort")
-plt.plot(x3, y3, label = "Timsort")
+plt.plot(x1, y1, label = "Quicksort", color = 'red')
+plt.plot(x2, y2, label = "Mergesort", color = 'blue')
+plt.plot(x3, y3, label = "Timsort", color = 'orange')
 plt.legend()
 plt.xlabel('N')
 plt.ylabel('Tid (nanosekunder)')
 plt.gcf().axes[0].yaxis.get_major_formatter().set_scientific(False)
-plt.title('')
+
+plt.title('Osorterad data')
 plt.show()
